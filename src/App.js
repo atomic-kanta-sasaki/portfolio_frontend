97e69340512dd37de07698d9b0b7fd9fad5a75e4
kanta_sasaki <k_padbjw@icloud.com>
2020-04-13 13:17:47 +0900

    componentの作成と動作テスト

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MenuList from '@material-ui/core/MenuList';
+import MenuItem from '@material-ui/core/MenuItem';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import Typography from '@material-ui/core/Typography';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+import PriorityHighIcon from '@material-ui/icons/PriorityHigh';
+import ProgressBar from './ProgressBar';
+import AppBar from './AppBar';
+import Avatar from './AppBar';
+import Card from './Card';
+import Skelton from './Skelton';
 
-function App() {
+const useStyles = makeStyles({
+  root: {
+    width: 230,
+  },
+});
+
+export default function TypographyMenu() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Paper className={classes.root}>
+      <MenuList>
+        <MenuItem>
+          <ListItemIcon>
+            <SendIcon fontSize="small" />
+          </ListItemIcon>
+          <Typography variant="inherit">A short message</Typography>
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <PriorityHighIcon fontSize="small" />
+          </ListItemIcon>
+          <Typography variant="inherit">A very long text that overflows</Typography>
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <DraftsIcon fontSize="small" />
+          </ListItemIcon>
+          <Typography variant="inherit" noWrap>
+            A very long text that overflows
+          </Typography>
+        </MenuItem>
+      </MenuList>
+      <ProgressBar />
+      <AppBar />
+      <Skelton />
+      <Card />
+      <Avatar />
+    </Paper>
+
   );
 }
-
-export default App;
