6e6f3cd809d597d72a6bf6f88d178b8f3dca8341
kanta_sasaki <k_padbjw@icloud.com>
2020-04-13 19:35:08 +0900

    不要コードの削除

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import MenuList from '@material-ui/core/MenuList';
-import MenuItem from '@material-ui/core/MenuItem';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import Typography from '@material-ui/core/Typography';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import SendIcon from '@material-ui/icons/Send';
-import PriorityHighIcon from '@material-ui/icons/PriorityHigh';
 import ProgressBar from './ProgressBar';
 import AppBar from './AppBar';
 import Avatar from './Avatar';
