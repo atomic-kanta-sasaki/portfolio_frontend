0d8d3e3101a21ff8f09266b530553dbb863431a5
kanta_sasaki <k_padbjw@icloud.com>
2020-04-16 22:23:44 +0900

    SNSIcon

--- src/Connect.js
+++ src/Connect.js
@@ -12,7 +12,9 @@ import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
     root: {
-        minWidth: 275,
+        width: '800px',
+        marginLeft: '20%',
+        marginTop: '2%',
     },
     bullet: {
         display: 'inline-block',
@@ -29,7 +31,7 @@ const useStyles = makeStyles({
 
 const IconPlace = styled.div`
     margin: 0 auto;
-    display: flexl;
+    display: flex;
 `;
 
 export default function SimpleCard() {
@@ -37,34 +39,14 @@ export default function SimpleCard() {
     const bull = <span className={classes.bullet}>•</span>;
 
     return (
-        // <Card className={classes.root}>
-        //     <CardActions>
-        //         <IconPlace>
-        //             <TwitterIcon />
-        //             <InstagramIcon />
-        //             <GithubIcon />
-        //         </IconPlace>
-        //     </CardActions>
-        // </Card>
         <Card className={classes.root}>
-            <CardContent>
-                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                    Word of the Day
-        </Typography>
-                <Typography variant="h5" component="h2">
-                    be{bull}nev{bull}o{bull}lent
-        </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                    adjective
-        </Typography>
-                <Typography variant="body2" component="p">
-                    well meaning and kindly.
-          <br />
-                    {'"a benevolent smile"'}
-                </Typography>
-            </CardContent>
             <CardActions>
-                <Button size="small">Learn More</Button>
+                <IconPlace>
+                    <h2>SNS →</h2>
+                    <TwitterIcon />
+                    <InstagramIcon />
+                    <GithubIcon />
+                </IconPlace>
             </CardActions>
         </Card>
     );
