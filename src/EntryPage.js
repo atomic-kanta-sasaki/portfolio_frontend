0eb076eeb4ba95ba9741435ba756a2d3b02c0222
kanta_sasaki <k_padbjw@icloud.com>
2020-04-18 08:52:12 +0900

    entry pageの作成

--- src/EntryPage.js
+++ src/EntryPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './Card.css';
 import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+import './EntryPage.css'
 
 const BackgroundImage = styled.body`
     background-image: url(/static/images/avatar/kanta.png);
@@ -10,15 +12,36 @@ const BackgroundImage = styled.body`
 `;
 
 const Contents = styled.div`
-    height: 1000px;
-    width: 1000px;
+    width: 100vw;
+    height: 100vh;
+`;
+const ButtonPosition = styled.div`
+    text-align: center;
+    padding-top: 38%;
+    &:hover {
+        transform: rotateX(360deg);
+    }
 `;
 
-export default function MediaCard() {
-    return (
-        <BackgroundImage>
-            <Contents>
-            </Contents>
-        </BackgroundImage>
-    );
+class EntryPage extends Component {
+
+    toMainPage() {
+        this.props.history.push('/About')
+    }
+
+    render() {
+        return (
+            <BackgroundImage>
+                <Contents>
+                    <ButtonPosition>
+                        <Button id="btn-1" onClick={this.toMainPage.bind(this)}>
+                            entry
+                        </Button>
+                    </ButtonPosition>
+                </Contents>
+            </BackgroundImage>
+        )
+    }
 }
+
+export default EntryPage;
