07de052f64cfafd0403b8f6f8824e4fcf723132a
kanta_sasaki <k_padbjw@icloud.com>
2020-04-18 06:17:23 +0900

    React Router導入

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
 import React, { Component } from 'react';
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
-import ProgressBar from './ProgressBar';
-import AppBar from './AppBar';
-import styled from 'styled-components';
-import Card from './TechIntroduction';
-import Introduction from './ Introduction';
-import Connect from './Connect';
-
-const IntroductionSpace = styled.div`
-    margin: 54px;
-    margin-top:24px;
-`;
-
-const classes = makeStyles({
-  root: {
-    width: '1400px',
-    margin: 'auto',
-    width: '90%',
-  },
-});
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Page from './Page';
+import EntryPage from './EntryPage';
 
 class App extends Component {
-  // classes = useStyles();
-
   render() {
     return (
-      <Paper className={classes.root}>
-        <ProgressBar />
-        <AppBar />
-        <IntroductionSpace>
-          <Introduction />
-        </IntroductionSpace>
-        <Card />
-        <Connect />
-      </Paper>
-    )
+      <div className="App">
+        <Router>
+          <div>
+            <Route exact path='/' component={EntryPage} />
+            <Route path='/About' component={Page} />
+          </div>
+        </Router>
+      </div>
+    );
   }
 }
 
