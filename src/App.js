a92dfd0a1ceaefc8f3ce58a2c88694e325077fad
kanta_sasaki <k_padbjw@icloud.com>
2020-04-19 05:00:30 +0900

    Routepassの追懐

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Page from './Page';
 import EntryPage from './EntryPage';
+import TechProfile from './TechProfile';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <div>
             <Route path='/' component={EntryPage} exact={true} />
             <Route path='/About' component={Page} />
+            <Route path='/TechProfile' component={TechProfile}></Route>
           </div>
         </Router>
       </div>
