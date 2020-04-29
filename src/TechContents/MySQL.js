4b49589a96216262be318da2ba04c2de973e7fca
kanta_sasaki <k_padbjw@icloud.com>
2020-04-29 10:46:23 +0900

    TechModalをclassComponent化

--- src/TechContents/MySQL.js
+++ src/TechContents/MySQL.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -7,51 +7,60 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import styled from 'styled-components';
 
-export default function AlertDialog() {
-    const [open, setOpen] = React.useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    const DialogStyled = styled.div`
+const DialogStyled = styled.div`
     `;
 
-    const Title = styled.div`
+const Title = styled.div`
         text-align: center;
         font-size: 25px;
     `;
 
-    return (
-        <DialogStyled>
-            <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-                Detail
+class MySQL extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    handleClickOpen() {
+        this.setState({ isOpen: true })
+    }
+
+    handleClickClose() {
+        this.setState({ isOpen: false })
+    }
+
+    render() {
+        return (
+            <DialogStyled>
+                <Button variant="outlined" color="primary" onClick={this.handleClickOpen.bind(this)}>
+                    Detail
             </Button>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    <Title>Kotlin</Title>
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        実務での使用経験あり.
-                        新機能開発の際のDBの追加作成, GET, POST, PUT, DELETEと一通りのクエリの作成経験あり
+                <Dialog
+                    open={this.state.isOpen}
+                    onClose={this.handleClickClose.bind(this)}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        <Title>MySQL</Title>
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            実務での使用経験あり.
+                            新機能開発の際のDBの追加作成, GET, POST, PUT, DELETEと一通りのクエリの作成経験あり
                     </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">
-                        Close
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClickClose.bind(this)} color="primary">
+                            Close
                     </Button>
-                </DialogActions>
-            </Dialog>
-        </DialogStyled>
-    );
+                    </DialogActions>
+                </Dialog>
+            </DialogStyled>
+        );
+    }
 }
+
+export default MySQL;
--- src/TechContents/Python.js
+++ src/TechContents/Python.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -7,53 +7,62 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import styled from 'styled-components';
 
-export default function AlertDialog() {
-    const [open, setOpen] = React.useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    const DialogStyled = styled.div`
+const DialogStyled = styled.div`
     `;
 
-    const Title = styled.div`
+const Title = styled.div`
         text-align: center;
         font-size: 25px;
     `;
 
-    return (
-        <DialogStyled>
-            <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-                Detail
+class Python extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    handleClickOpen() {
+        this.setState({ isOpen: true })
+    }
+
+    handleClickClose() {
+        this.setState({ isOpen: false })
+    }
+
+    render() {
+        return (
+            <DialogStyled>
+                <Button variant="outlined" color="primary" onClick={this.handleClickOpen.bind(this)}>
+                    Detail
             </Button>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    <Title>Python</Title>
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Djangoを用いたWebアプリケーションを個人で作成.
-                        研究でのデータ解析, スクレイピング, 東京都市大学新聞会で使用しているTwitterのBotの作成など, 作業効率化や
+                <Dialog
+                    open={this.state.isOpen}
+                    onClose={this.handleClickClose.bind(this)}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        <Title>Python</Title>
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Djangoを用いたWebアプリケーションを個人で作成.
+                            研究でのデータ解析, スクレイピング, 東京都市大学新聞会で使用しているTwitterのBotの作成など, 作業効率化や
                         自動化などをする際に積極的に利用. <br />
-                        実務経験はスクレイピングを少した程度.
+                            実務経験はスクレイピングを少した程度.
                     </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">
-                        Close
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClickClose.bind(this)} color="primary">
+                            Close
                     </Button>
-                </DialogActions>
-            </Dialog>
-        </DialogStyled>
-    );
+                    </DialogActions>
+                </Dialog>
+            </DialogStyled>
+        );
+    }
 }
+
+export default Python;
