dd202daf4bf9babf4965376f1d865d9bb74acc53
kanta_sasaki <k_padbjw@icloud.com>
2020-04-29 10:04:32 +0900

    past workの

--- src/DialogContent/enPit.js
+++ src/DialogContent/enPit.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -7,54 +7,63 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import styled from 'styled-components';
 
-export default function AlertDialog() {
-    const [open, setOpen] = React.useState(false);
+const DialogStyled = styled.div`
+`;
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    const DialogStyled = styled.div`
-    `;
-
-    const Title = styled.div`
+const Title = styled.div`
         text-align: center;
         font-size: 25px;
-    `;
+`;
 
-    return (
-        <DialogStyled>
-            <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-                Detail
-            </Button>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    <Title>PARS(enPiT)</Title>
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Kotlin, SQLiteを使用.<br />
-                        Kotlinを使用した位置情報を利用した予定管理アプリを作成.<br />
-                        このアプリの特徴としては, 時間で予定通知を出すのではなく, 登録した場所に近づいたら通知を出すというもの.<br />
-                        またPMとして未経験者でも開発できるような環境のサポートや技術的サポートを実施.
+class EnPit extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    handleClickOpen() {
+        this.setState({ isOpen: true })
+    }
+
+    handleClickClose() {
+        this.setState({ isOpen: false })
+    }
+
+    render() {
+        return (
+            <DialogStyled>
+                <Button variant="outlined" color="primary" onClick={this.handleClickOpen.bind(this)}>
+                    Detail
+                </Button>
+                <Dialog
+                    open={this.state.isOpen}
+                    onClick={this.handleClickClose.bind(this)}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        <Title>PARS(enPiT)</Title>
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Kotlin, SQLiteを使用.<br />
+                            Kotlinを使用した位置情報を利用した予定管理アプリを作成.<br />
+                            このアプリの特徴としては, 時間で予定通知を出すのではなく, 登録した場所に近づいたら通知を出すというもの.<br />
+                            またPMとして未経験者でも開発できるような環境のサポートや技術的サポートを実施.
 
                     </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">
-                        Close
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClickClose.bind(this)} color="primary">
+                            Close
                     </Button>
-                </DialogActions>
-            </Dialog>
-        </DialogStyled>
-    );
+                    </DialogActions>
+                </Dialog>
+            </DialogStyled>
+        );
+    }
 }
+
+export default EnPit;
