7258b05ba6584cf215b3fa22ca07bb412ba5b3b8
kanta_sasaki <k_padbjw@icloud.com>
2020-04-29 10:14:12 +0900

    PastWorkのモーダルをclassComponentにリファクタリング

--- src/DialogContent/LabBase.js
+++ src/DialogContent/LabBase.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -7,52 +7,60 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import styled from 'styled-components';
 
-export default function AlertDialog() {
-    const [open, setOpen] = React.useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    const DialogStyled = styled.div`
+const DialogStyled = styled.div`
     `;
 
-    const Title = styled.div`
+const Title = styled.div`
         text-align: center;
         font-size: 25px;
     `;
+class LabBase extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        };
+    }
 
-    return (
-        <DialogStyled>
-            <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-                Detail
-            </Button>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    <Title>LabBase</Title>
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Java, React, MySQL, Docker, PlayFrameworkを利用.<br />
-                        主にサーバーサイドの新機能開発, 不具合修正, アーキテクチャ刷新作業を行っていた.<br />
-                        フロントエンドは不具合の修正や小規模のUIリプレイスを担当.
+    handleClickOpen() {
+        this.setState({ isOpen: true })
+    }
+
+    handleClickClose() {
+        this.setState({ isOpen: false })
+    }
+
+    render() {
+        return (
+            <DialogStyled>
+                <Button variant="outlined" color="primary" onClick={this.handleClickOpen.bind(this)}>
+                    Detail
+                </Button>
+                <Dialog
+                    open={this.state.isOpen}
+                    onClick={this.handleClickClose.bind(this)}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        <Title>LabBase</Title>
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Java, React, MySQL, Docker, PlayFrameworkを利用.<br />
+                            主にサーバーサイドの新機能開発, 不具合修正, アーキテクチャ刷新作業を行っていた.<br />
+                            フロントエンドは不具合の修正や小規模のUIリプレイスを担当.
                     </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">
-                        Close
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClickClose.bind(this)} color="primary">
+                            Close
                     </Button>
-                </DialogActions>
-            </Dialog>
-        </DialogStyled>
-    );
+                    </DialogActions>
+                </Dialog>
+            </DialogStyled>
+        );
+    }
 }
+
+export default LabBase;
--- src/DialogContent/LabBaseX.js
+++ src/DialogContent/LabBaseX.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -7,52 +7,60 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import styled from 'styled-components';
 
-export default function AlertDialog() {
-    const [open, setOpen] = React.useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    const DialogStyled = styled.div`
+const DialogStyled = styled.div`
     `;
 
-    const Title = styled.div`
+const Title = styled.div`
         text-align: center;
         font-size: 25px;
     `;
+class LabBaseX extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        };
+    }
 
-    return (
-        <DialogStyled>
-            <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-                Detail
-            </Button>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">
-                    <Title>LabBaseX</Title>
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Pythonを利用<br />
-                        LabBaseXで使用した, 初期のシーズデータと研究者情報のスクレイピングを実施.
+    handleClickOpen() {
+        this.setState({ isOpen: true })
+    }
+
+    handleClickClose() {
+        this.setState({ isOpen: false })
+    }
+
+    render() {
+        return (
+            <DialogStyled>
+                <Button variant="outlined" color="primary" onClick={this.handleClickOpen.bind(this)}>
+                    Detail
+                </Button>
+                <Dialog
+                    open={this.state.isOpen}
+                    onClick={this.handleClickClose.bind(this)}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        <Title>LabBaseX</Title>
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Pythonを利用<br />
+                            LabBaseXで使用した, 初期のシーズデータと研究者情報のスクレイピングを実施.
 
                     </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">
-                        Close
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClickClose.bind(this)} color="primary">
+                            Close
                     </Button>
-                </DialogActions>
-            </Dialog>
-        </DialogStyled>
-    );
+                    </DialogActions>
+                </Dialog>
+            </DialogStyled>
+        );
+    }
 }
+
+export default LabBaseX;
